9161dfe98fcaf01f44a3b127cf41217a042b69af
martincuervo33 <74837897+martincuervo33@users.noreply.github.com>
2020-11-21 18:05:31 -0300

    Update Prueba.java

--- src/main/Prueba.java
+++ src/main/Prueba.java
@@ -4,7 +4,9 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba commits");
-		System.out.println("The Nameless");
+		System.out.println("converso");
+		System.out.println("hola como va");
+	
 	}
 
 }
